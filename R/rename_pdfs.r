c74f8658dc10e84cbb85d04f43fe2762528b2d11
Bret Alexander Beheim <beheim@gmail.com>
2024-05-15 14:59:14 +0200

    modified rename_pdfs to allow for PDF extensions and longer hash names

--- R/rename_pdfs.r
+++ R/rename_pdfs.r
@@ -1,14 +1,14 @@
 
 rename_pdfs <- function(path) {
 
-  files <- list.files(path, pattern="*.pdf", full.names = TRUE, recursive = FALSE)
+  files <- list.files(path, pattern="*.pdf|*.PDF", full.names = TRUE, recursive = FALSE)
 
   rename_counter <- 0
   unchanged_counter <- 0
 
   for(i in 1:length(files)){
       hash <- digest(files[i], algo = "sha1", file = TRUE)
-      hash_name <- paste(substr(hash, 1, 7) ,".pdf", sep = "") 
+      hash_name <- paste(substr(hash, 1, 11) ,".pdf", sep = "") 
       hash_name <- file.path(path, hash_name)
       if(files[i] != hash_name){ 
           file.rename(files[i], hash_name)
